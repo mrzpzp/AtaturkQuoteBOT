5e9483f29f10f27e5cba6c978bf3751f03ce842d
mrzpzp <85396359+mrzpzp@users.noreply.github.com>
2021-10-16 16:49:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,16 +21,6 @@ def login():
 
     return reddit
 
-def get_saved_comments():
-    if not os.path.isfile("replied_to.txt"):
-        replied_to = []
-    else:
-        with open("replied_to.txt", "r") as f:
-            replied_to = f.read()
-            replied_to = replied_to.split("\n")
-            replied_to = list(filter(None, replied_to))
-
-    return replied_to
 
 def reply(p_type):
   
@@ -48,49 +38,48 @@ def reply(p_type):
   else:
     random_index2 = random.randint(0, len(photosl.photos) - 1)
 
-    p_type.reply("Merhaba ben bir botum ve paylaşımında 'Atatürk'geçtiği için geldim.Senin için bir tane " + choose_from[random_index2] + " paylaştım. "+ " \n\n "
+    p_type.reply("Merhaba ben bir botum ve paylaşımında 'Atatürk' geçtiği için geldim.Senin için bir tane " + choose_from[random_index2] + " paylaştım. "+ " \n\n "
                                                     
                                     "^(I am a bot and this action was performed automatically.)" + "\n\n" + "[INFO|BİLGİ](https://www.reddit.com/user/mkemalataturk/comments/pcp0q0/ataturkquotebot_says_hellooo/)")
             
 
   print("Replied to " + p_type.id)
 
-  replied_to.append(p_type.id)
 
-  with open("replied_to.txt", "a") as f:
-    f.write(p_type.id + "\n")
-      
+def main(reddit):
 
-def main(reddit, replied_to):
-
-  subreddit = reddit.subreddit('Ata+Turkey+TurkeyJerky+KGBTR+ArsivUnutmaz+AteistTurk+BLKGM+Turkmenistan+Otuken')
+  subreddit = reddit.subreddit('Ata+Turkey+TurkeyJerky+KGBTR+ArsivUnutmaz+AteistTurk+BLKGM+Turkmenistan+Otuken+MuslumanTurk+Tiele')
       
   print("Collecting  last 3 mentions,last 50 comments and last 10 posts from new...")
 
   time.sleep(1)
   for mention in reddit.inbox.mentions(limit = 3):
-    if  mention.id not in replied_to and mention.author != reddit.user.me():
+    if  mention.new and mention.author != reddit.user.me():
+      mention.mark_read()
       time.sleep(1)
-      if mention.new:
-        reply(mention)
-        mention.mark_read()
+      reply(mention)
+  
       
   for submission in subreddit.new(limit=10):
     submission_lower = submission.title.lower()
-    submission.id = "t3_" + submission.id
-    if "atatürk" in submission_lower and submission.id not in replied_to and submission.author != reddit.user.me():
-      time.sleep(1)
-      reply(submission)
-
-  yes_or_no = random.randint(0,3)
-  for comment in subreddit.comments(limit=50):
-    if yes_or_no == 1:
-      comment_lower = comment.body.lower()
-      if "atatürk" in comment_lower and comment.id not in replied_to  and comment.author != reddit.user.me():
+    if submission.score >= 7:
+        
+      if "atatürk" in submission_lower and submission.saved is False and submission.author != reddit.user.me():
         time.sleep(1)
-        reply(comment)    
+        reply(submission)
+        submission.save()
 
 
+  for comment in subreddit.comments(limit=50):
+    comment_lower = comment.body.lower()
+    if comment.score >= 5:
+      
+      if "atatürk" in comment_lower and comment.saved is False and comment.author != reddit.user.me():
+        time.sleep(1)
+        reply(comment)  
+        comment.save()
+
+        
   print("Collect Completed.")
 
   print("Sleeping for 300 seconds...")
@@ -98,12 +87,11 @@ def main(reddit, replied_to):
 
  
 reddit = login()
-replied_to = get_saved_comments()
 
 
 while True:
   try:
-    main(reddit, replied_to)
+    main(reddit)
   except Exception as e:
     print(str(e) + " ,sleeping 120 seconds...")
     time.sleep(120)
